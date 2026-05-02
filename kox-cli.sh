f0569b38ac1bb55b7439db5c1e1e3adf15d0c51d
No name <132608038+nonamenebula@users.noreply.github.com>
2026-05-03 00:17:42 +0300

    feat(cli): bump version to 2026.05.02.14

--- kox-cli.sh
+++ kox-cli.sh
@@ -2,7 +2,7 @@
 # KOX Shield Management Console
 # https://kox.nonamenebula.ru | t.me/PrivateProxyKox
 
-KOX_VERSION="2026.05.02.13"
+KOX_VERSION="2026.05.02.14"
 
 CONF="/opt/etc/xray/config.json"
 KOXCONF="/opt/etc/xray/kox.conf"
