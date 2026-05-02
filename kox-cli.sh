d25778f63968d825e365f71f14d4c80bdb7c8473
Konstantin <kox0007@gmail.com>
2026-05-02 22:13:54 +0300

    chore: bump version to 2026.05.02.5

--- kox-cli.sh
+++ kox-cli.sh
@@ -2,7 +2,7 @@
 # KOX Shield Management Console
 # https://kox.nonamenebula.ru | t.me/PrivateProxyKox
 
-KOX_VERSION="2026.05.02.4"
+KOX_VERSION="2026.05.02.5"
 
 CONF="/opt/etc/xray/config.json"
 KOXCONF="/opt/etc/xray/kox.conf"
